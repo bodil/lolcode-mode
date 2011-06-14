f5af77506dfe255894e5d4622ece9b741bce4f79
Bodil Stokke <bodil@bodil.tv>
2011-06-14 16:08:23 +0200

    Added an example lolcode-mode + auto-complete configuration.

--- lolcode-mode.el
+++ lolcode-mode.el
@@ -44,6 +44,23 @@
 
 ;;   http://icanhaslolcode.org/
 
+;;; Configuration
+
+;; This is an example setup which integrates lolcode-mode with
+;; auto-complete-mode and yasnippet. It also sets default indentation
+;; to 2 spaces.
+
+;;   (require 'lolcode-mode)
+;;   (require 'auto-complete)
+;;   (defvar ac-source-lolcode
+;;     '((candidates . lolcode-lang-all)))
+;;   (add-to-list 'ac-modes 'lolcode-mode)
+;;   (add-hook 'lolcode-mode-hook
+;;             (lambda ()
+;;               (setq default-tab-width 2)
+;;               (add-to-list 'ac-sources 'ac-source-lolcode)
+;;               (add-to-list 'ac-sources 'ac-source-yasnippet)))
+
 ;;; Code:
 
 (defgroup lolcode-mode nil
